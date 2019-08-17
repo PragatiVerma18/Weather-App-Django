c84317f43fe285ef4e62dc99932c95583190a8a7
pragati <itispragativerma@gmail.com>
2019-08-17 20:36:13 +0530

    added multiple city weather

--- weather/views.py
+++ weather/views.py
@@ -1,20 +1,33 @@
 import requests
 from django.shortcuts import render
-
+from .models import City
+from .forms import CityForm
 
 def index(request):
-  url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=d30eb513f3e2d8a6996e9d83bfcdd779'
-  city = 'London'
-    
-  r = requests.get(url.format(city)).json()
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=d30eb513f3e2d8a6996e9d83bfcdd779'
 
-  city_weather = {
-            'city' : city,
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        form.save()
+
+    form = CityForm()
+
+    cities = City.objects.all()
+
+    weather_data = []
+
+    for city in cities:
+
+        r = requests.get(url.format(city)).json()
+
+        city_weather = {
+            'city' : city.name,
             'temperature' : r['main']['temp'],
             'description' : r['weather'][0]['description'],
             'icon' : r['weather'][0]['icon'],
         }
 
+        weather_data.append(city_weather)
 
-  context = {'city_weather' : city_weather}
-  return render(request, 'weather/weather.html', context)
+    context = {'weather_data' : weather_data, 'form' : form}
+    return render(request, 'weather/weather.html', context)
